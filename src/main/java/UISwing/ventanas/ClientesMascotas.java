cb5ff252493b42cd7bcd54e5fd98c190cb43e776
ArturoVazquez <arturovazquez1995@gmail.com>
2024-03-17 17:37:35 +0100

    PanelClienteMascota
    falta tabla clientes/mascotas

---
+++ src/main/java/UISwing/ventanas/ClientesMascotas.java
@@ -0,0 +1,16 @@
+package UISwing.ventanas;
+
+import javax.swing.JPanel;
+
+public class ClientesMascotas extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public ClientesMascotas() {
+
+	}
+
+}
