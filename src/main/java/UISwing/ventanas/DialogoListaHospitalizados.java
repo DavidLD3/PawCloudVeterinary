db9092eed8afac143b90c0d1cfac49760fe93460
DavidLD3 <117693416+DavidLD3@users.noreply.github.com>
2024-03-20 19:49:07 +0100

    pruebaa

--- src/main/java/UISwing/ventanas/DialogoListaHospitalizados.java
+++ src/main/java/UISwing/ventanas/DialogoListaHospitalizados.java
@@ -33,7 +33,7 @@ public class DialogoListaHospitalizados extends JDialog implements Actualizacion
     // prueba de gi  
     //prueba de git3
     //prueba git
-    
+    // prueba 2323
     class PaddingTableCellRenderer extends DefaultTableCellRenderer {
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
