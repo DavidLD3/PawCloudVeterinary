dc3f783fe5d976528ab8cce437e67a1f9171df12
ArturoVazquez <arturovazquez1995@gmail.com>
2024-04-18 03:25:56 +0200

    almacen

--- src/main/java/UISwing/ventanas/PanelAlmacen.java
+++ src/main/java/UISwing/ventanas/PanelAlmacen.java
@@ -1,6 +1,7 @@
 package UISwing.ventanas;
 
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class PanelAlmacen extends JPanel {
 
@@ -10,7 +11,15 @@ public class PanelAlmacen extends JPanel {
 	 * Create the panel.
 	 */
 	public PanelAlmacen() {
+		setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 1112, 45);
+		add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 50, 1112, 603);
+		add(panel);
 
 	}
-
 }
